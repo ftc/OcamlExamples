ab8ec59a97221042a5361ab4a449d69127b79b3b
Shawn Meier <shawn.meier@gmail.com>
2014-09-18 13:36:25 -0600

    added fold_right example

--- fold_examples.ml
+++ fold_examples.ml
@@ -5,3 +5,6 @@ let v = List.fold_left (fun x y -> x+y) 0 [1;2;3;4] in
 (*convert list of numbers to string and concat*)
 let v = List.fold_left ( fun x y -> x^(Pervasives.string_of_int y)) "" [1;2;3;4] in
   Format.printf "%s\n" v;;
+
+let v = List.fold_right (fun y x -> x^(Pervasives.string_of_int y)) [1;2;3;4] "" in
+  Format.printf "%s\n" v;;
