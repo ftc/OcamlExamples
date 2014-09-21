bfbc2e27858e4fe5f37f75b367fe7737f86a3282
Shawn Meier <shawn.meier@gmail.com>
2014-09-21 17:04:54 -0600

    added multi update to record example

--- record_example.ml
+++ record_example.ml
@@ -1,6 +1,7 @@
-type foo = {a : string; b: int;}
+type foo = {a : string; b: int; c: string}
 
+(*let _ = ... required since type cannot be followed by expr unless let binding*)
 let _ = 
-  let v = {a="hi"; b=1} in
-  let r = {v with a="bye"} in
+  let v = {a="hi"; b=1; c="oooo!"} in
+  let r = {v with a="bye" ;b = 1337} in
   Format.printf "%s %i\n" r.a r.b;;
